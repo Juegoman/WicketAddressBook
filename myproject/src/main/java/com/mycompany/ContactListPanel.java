7b1e6f79954bcad789bba675da87e081d3b67d1e
Ruben Gallegos <gallegos_ruben@yahoo.com>
2017-01-23 00:28:30 -0700

    Working on reading and updating functionality for contacts. Still non-functional, but getting very close.

--- myproject/src/main/java/com/mycompany/ContactListPanel.java
+++ myproject/src/main/java/com/mycompany/ContactListPanel.java
@@ -8,17 +8,21 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.wicket.Session;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.CompoundPropertyModel;
 import redis.clients.jedis.Jedis;
 
 public class ContactListPanel extends Panel {
     private List<Contact> contacts;
+    private ListView contactList;
     
     public ContactListPanel(String id) {
         super(id);
         
         List<byte[]> contactByteList = null;
-        contacts = new ArrayList<Contact>();
+        contacts = new ArrayList<>();
         
         try(Jedis jedis = WicketApplication.jedisPool.getResource()) {
             String keyStr = WicketApplication.KEYPREFIX + Session.get().getAttribute("username");
@@ -27,6 +31,7 @@ public class ContactListPanel extends Panel {
         
         if (contactByteList == null || contactByteList.isEmpty()) {
             addOrReplace(new Label("alertInfo", "There are no contacts. Why don't you add some?"));
+            addOrReplace(new Label("contactList", ""));
         } else {
             contactByteList.forEach(contactBytes -> {
                 Contact contact = new Contact();
@@ -38,7 +43,17 @@ public class ContactListPanel extends Panel {
                 contacts.add(contact);
             });
             addOrReplace(new Label("alertInfo", "There are " + contacts.size() + "contacts."));
-            
+            contactList = new ListView<Contact>("contactList", contacts) {
+                @Override
+                protected void populateItem(ListItem<Contact> contact) {
+                    contact.setDefaultModel(new CompoundPropertyModel<>(contact.getModelObject()));
+                    contact.add(new Label("fullName"));
+                    contact.add(new Label("address"));
+                    contact.add(new Label("email"));
+                    contact.add(new Label("phone"));
+                    contact.add(new UpdateContactForm("updateContactForm", (CompoundPropertyModel) contact.getDefaultModel()));
+                }
+            };
         }
     }
     
